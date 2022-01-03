39cff75107767402d29848fe07381d93b3c30821
evanweenen <evanweenen@ethz.ch>
2022-01-03 17:11:39 +0100

    Update setup with long description

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup
 
-setup(name='bike2csv',
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='bike2csv',
     version='0.1',
-    description='Convert FIT, PWX and TCX files from a bike computer to CSV',
-    url='https://github.com/evavanweenen/bike2csv',
     author='Eva van Weenen',
     author_email='evanweenen@ethz.ch',
+    description='Convert FIT, PWX and TCX files from a bike computer to CSV',
+    long_description=long_description,
+    long_dscription_content_type='text/markdown',
+    url='https://github.com/evavanweenen/bike2csv',
     license='MIT',
     packages=['bike2csv'],
-    scripts=['bin/run'],
+    scripts=['bin/run.py'],
     zip_safe=False,
-    install_requires=['fitparse'])
+    install_requires=['fitparse']
+    )
